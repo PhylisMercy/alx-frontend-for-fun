9e1a2f6f82ed9aa8d627d6e387a5bf9986e139bf
PhylisMercy <phylis.mercy42@gmail.com>
2023-07-18 10:26:14 +0000

    Improve markdown2html.py by parsing Headings Markdown syntax for generating HTML

--- markdown2html.py
+++ markdown2html.py
@@ -10,13 +10,25 @@ Usage: ./markdown2html.py README.md README.html
 
 import sys
 import os
+import re
 
 def convert_to_html(markdown_file, output_file):
     """
     Convert the given Markdown file to HTML and save it to the output file.
     """
-    # Add your code here for Markdown to HTML conversion
-    pass
+    with open(markdown_file, 'r') as md_file:
+        markdown_content = md_file.read()
+
+    # Replace heading syntax with HTML tags
+    html_content = re.sub(r'^# (.+)$', r'<h1>\1</h1>', markdown_content, flags=re.MULTILINE)
+    html_content = re.sub(r'^## (.+)$', r'<h2>\1</h2>', html_content, flags=re.MULTILINE)
+    html_content = re.sub(r'^### (.+)$', r'<h3>\1</h3>', html_content, flags=re.MULTILINE)
+    html_content = re.sub(r'^#### (.+)$', r'<h4>\1</h4>', html_content, flags=re.MULTILINE)
+    html_content = re.sub(r'^##### (.+)$', r'<h5>\1</h5>', html_content, flags=re.MULTILINE)
+    html_content = re.sub(r'^###### (.+)$', r'<h6>\1</h6>', html_content, flags=re.MULTILINE)
+
+    with open(output_file, 'w') as html_file:
+        html_file.write(html_content)
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
